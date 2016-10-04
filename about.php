2e7c28947d8194b021fe259195bd17d1d11aeb1a
Nikita Thakur <Nikita Thakur>
2016-10-04 22:54:57 +0530

    Initial Commit

---
+++ about.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>TheRedDevils|About</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+   
+
+    <style type="text/css">
+  div.img-responsive a:hover {
+   margin: 5px;
+
+    border: 3px solid black;
+    height: auto;
+    width: auto;
+    float: left;
+    text-align: center;
+    box-shadow: 5px 5px 5px #191970;
+    
+
+    -ms-transform: scale(1.3,1.3); 
+    -webkit-transform: scale(1.3,1.3); 
+    transform: scale(1.3,1.3);
+
+}
+    </style>
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">TheRedDevils</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="about.php">About</a>
+                    </li>
+                    <li>
+                        <a href="fixtures.php">Fixtures</a>
+                    </li>
+                    <li>
+                        <a href="tickets.php">Tickets</a>
+                    </li>
+                     <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Squad <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="currsquad.php">Current Squad 2016-17</a>
+                            </li>
+                            <li>
+                                <a href="retleg.php">Retired/Legends</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact Us</a>
+                    </li>
+                   
+                   
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">About
+                    <small></small>
+                </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+            <div class="col-md-6">
+                <img class="img-responsive", src="images/mufcbg.png" alt="">
+            </div>
+            <div class="mark">
+                <h2>About Our Club</h2>
+                <p>Manchester United Football Club was formed in 1878 as Newton Heath LYR Football Club by the Carriage and Wagon department of the Lancashire and Yorkshire Railway depot at Newton Heath.[1] The team initially played games against other departments and rail companies at their home ground at North Road, but by 1888 the club had become a founding member of The Combination, a regional football league. However, following the league's dissolution before the end of its first season, Newton Heath joined the newly formed Football Alliance, which ran for three seasons before being merged with The Football League. This resulted in the club starting the 1892–93 season in the First Division, by which time it had become independent of the rail company, dropped the "LYR" from its name and moved to a new ground at Bank Street.[1] After just two seasons, the club was relegated to the Second Division.[1]
+
+In January 1902, with debts of £2,670 – equivalent to £260,000 in 2016[2] – the club was served with a winding-up order.[3] Captain Harry Stafford found four local businessmen – including John Henry Davies, who became club president – each willing to invest £500 in return for a direct interest in running the club.[4] As a mark of this fresh start, on 24 April 1902, the club's name was changed to "Manchester United".[5][6] Under Ernest Mangnall, who became club secretary in 1903, the team finished as Second Division runners-up in 1906 and secured promotion to the First Division, which it won in 1908 – the club's first league title. The following season began with victory in the first ever Charity Shield[7] and ended with the club's first FA Cup title. Manchester United moved to a new stadium at Old Trafford in 1910, and won the First Division for the second time in 1911, but at the end of the following season, secretary Mangnall left to join Manchester City.[8]
+
+In 1922, three years after the resumption of football following the First World War, the club was relegated to the Second Division, where it remained until regaining promotion in 1925. Relegated again in 1931, Manchester United became a yo-yo club, achieving its all-time lowest position of 20th place in the Second Division in 1934. Following the death of John Henry Davies in October 1927, the club's finances deteriorated to the extent that Manchester United would likely have gone bankrupt had it not been for an investment of £2,000 in December 1931 by James W. Gibson, who assumed control of the club.[9] In the 1938–39 season – the last year of football before the Second World War – the club finished 14th in the First Division.[9] During the war, the club participated in the Wartime League and the Football League War Cup, but in 1941, Old Trafford was damaged by German bombs and would not be fully repaired until 1949.[10]</p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- coaching staff -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Coaching Staff</h2>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 text-center">
+                <div class="thumbnail">
+                    <a href=""><img class="img-responsive" src="trd7.png" alt=""></a>
+                    <div class="caption">
+                        <h3>Jose Mourinho<br>
+                            <small>Manager</small>
+                        </h3>
+                        <p>2 Premier League titles and 2 UCL titles</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+              <div class="col-md-2 col-sm-4 col-xs-6 text-center">
+                <div class="thumbnail" style="max-height: 450px">
+                    <img class="img-responsive" src="c5.png" alt="">
+                    <div class="caption">
+                        <h3>Giovanni Cerra<br>
+                            <small>Fitness instructor</small>
+                        </h3>
+                        <p>Worked for Argentina National Team</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+              <div class="col-md-2 col-sm-4 col-xs-6 text-center" >
+                <div class="thumbnail" style="height: 450px">
+                    <img class="img-responsive" src="c4.png" alt="">
+                    <div class="caption">
+                        <h3>Emilio Alvarez<br>
+                            <small>Defence coach</small>
+                        </h3>
+                        <p>Trained at AC Milan</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+              <div class="col-md-2 col-sm-4 col-xs-6 text-center" >
+                <div class="thumbnail" style="height: 450px">
+                    <img class="img-responsive" src="c3.png" alt="">
+                    <div class="caption">
+                        <h3>Carlos Lalin<br>
+                            <small>Medical Head</small>
+                        </h3>
+                        <p>Trained Brazil natonal team</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+            <div class="col-md-2 col-sm-4 col-xs-6 text-center" >
+                <div class="thumbnail" style="height: 450px">
+                    <img class="img-responsive" src="trd5.png" alt="" >
+                    <div class="caption">
+                        <h3>S. Louro<br>
+                            <small>Goalkeeping coach</small>
+                        </h3>
+                        <p>Trained Gianluigi Buffon</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6 text-center" >
+                <div class="thumbnail" style="height: 450px">
+                    <img class="img-responsive" src="trd4.png" alt="">
+                    <div class="caption">
+                        <h3>Rui Faria<br>
+                            <small>Mid-field Coach</small>
+                        </h3>
+                        <p>Former French Mid-fielder</p>
+                        <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+           </div>
+        <!-- /.row -->
+
+        <!-- Our achievements -->
+        <div class="well">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Achievements</h2>
+            </div>
+           
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/ucl.png" alt="">
+                <p align='center'>UCL X3</p>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/eplt.png" alt="">
+                <p align='center'>PREMIER LEAGUE X20</p>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/cwc.png" alt="">
+                <p align='center'>CLUB WORLD CUPX1</p>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/fa.png" alt="">
+                <p align='center'>FA CUP X12</p>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/super.png" alt="">
+                <p align='center'>UEFA Super CUPX1</p>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/intlcl.png" alt="">
+                <p align='center'>Intercontinental CUPX1</p>
+            </div>
+        </div></div>
+        <!-- /.row -->
+
+   
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; TheRedDevils</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,304 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>TheRedDevils|Home</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  
+</head>
+
+       
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">TheRedDevils</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="about.php">About</a>
+                    </li>
+                    <li>
+                        <a href="fixtures.php">Fixtures</a>
+                    </li>
+                    <
+                     <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Squad <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="currsquad.php">Current Squad 2016-17</a>
+                            </li>
+                            <li>
+                                <a href="retleg.php">Retired/Legends</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    
+                      <li>
+                        <a href="gallery.php">Gallery</a>
+                    </li>
+
+                    <li>
+                        <a href="contact.php">Contact Us</a>
+                    </li>
+                   
+                 
+                   
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Header Carousel -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+               <div class="fill" style="background-image:url('images/zlatan.png');"></div>
+                <div class="carousel-caption">
+                    <h2>Ibrahimovic signs for United as a free agent</h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('images/pogba.png');"></div>
+                <div class="carousel-caption">
+                    <h2>Look who's back at United!</h2>
+                </div>
+            </div>
+            <div class="item">
+                <div class="fill" style="background-image:url('images/adidas.png');"></div>
+                <div class="carousel-caption">
+                    <h2>United to sign 200m Euro sponsorship deal with Adidas</h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+    </header>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Marketing Icons Section -->
+        <div class="row" style='width: 100%'>
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Welcome to TheRedDevils
+                </h1>
+           
+        <!-- League table -->  
+        <div class='well'>
+        <div class="row" >
+        <div class="col-sm-4">
+        <iframe src="http://www.tablesleague.com/iframe?width=700&height=420&font_name=Tahoma&position=1&font_size=12&team_link=1&link_color=404040&games=1&wins=1&draws=1&lost=1&goals=1&goals_against=1&gd=1&points=1&next=1&form=1&font_size=12&font_color=0f0a0a&bg_color=FFFFFF&header_font_color=FFFFFF&header_bg_color=165c70&bg_col=960707&font_color_col=FFFFFF&highlight=ede6e6&hover=edd02f&league_header=1&league=l_145&team=1509&timezone=5.5&language=2&team_flags=0" width="680" height="400" frameborder="0" scrolling="no"></iframe>
+        </div>
+        <div class="col-sm-4"></div>
+        <!--upcoming fixtures-->
+        <div class="col-sm-4" style="padding: 0 10px 0 10px;">
+        <h2 style="padding-left: 25px"><b>Upcoming Fixtures</b></h2>
+
+    <div class="panel panel-default">
+     
+      <div class="panel-body">
+       <a href="#" data-toggle="tooltip" title="manchester united vs chelsea">
+      <div class="row">
+      <div class="col-xs-5"><img src="images/muicon.png" style="padding-left: 30px;"/></div>
+      <div class="col-xs-2"><h3><b>:</b></h3></div>
+      <div class="col-xs-5"><img src="images/chicon.png" style="padding-right: 30px"/></div>
+      <div class="col-xs-5"></div>
+    
+      </div></a>
+      <script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
+
+    </div>
+    </div>
+    <div class="panel panel-default">   
+      
+      <div class="panel-body">
+        <a href="#" data-toggle="tooltip" title="manchester united vs chelsea">
+      <div class="row">
+      <div class="col-xs-5"><img src="images/muicon.png" style="padding-left: 30px;"/></div>
+      <div class="col-xs-2"><h3><b>:</b></h3></div>
+      <div class="col-xs-5"><img src="images/chicon.png" style="padding-right: 30px"/></div>
+      <div class="col-xs-5"></div>
+    
+      </div></a>
+      <script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
+
+      </div>
+    </div>
+    <div class="panel panel-default">
+      
+      <div class="panel-body">
+            <a href="#" data-toggle="tooltip" title="manchester united vs chelsea">
+      <div class="row">
+      <div class="col-xs-5"><img src="images/muicon.png" style="padding-left: 30px;"/></div>
+      <div class="col-xs-2"><h3><b>:</b></h3></div>
+      <div class="col-xs-5"><img src="images/chicon.png" style="padding-right: 30px"/></div>
+      <div class="col-xs-5"></div>
+    
+      </div></a>
+      <script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+</script>
+      </div>
+    </div>
+    <div class="row"><a href="" style="padding-left: 150px"><b>MORE...</b></a></div>
+  </div>
+           
+        </div>
+
+        </div>
+        
+        </div>     
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                  
+                </div>
+            <ul class="list-inline">
+                            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+                            </li>
+                        </ul>
+            </div>
+
+            <!-- /.row -->
+
+        <!-- Our sponsors -->
+        <div class='well'>
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Sponsors</h2>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/adi.png" alt="">
+              <p align='center'></p>
+             </div> 
+
+
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/gulf.png" alt="">
+                <p align='center'></p>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/aon.png" alt="">
+                <p align='center'></p>
+            </div>
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/chev.png" alt="">
+                <p align='center'></p>
+                </div>
+             <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/hcl.png" alt="">
+                <p align='center'></p>
+            </div>
+            
+            <div class="col-md-2 col-sm-4 col-xs-6">
+                <img class="img-responsive customer-img" src="images/ea.png" alt="">
+                <p align='center'></p>
+            </div>
+
+         </div>   
+        </div></div>
+        <!-- /.row -->
+          <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; TheRedDevils</p>
+                </div>
+            </div>
+
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
